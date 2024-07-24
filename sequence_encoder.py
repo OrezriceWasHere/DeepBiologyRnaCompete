1fcf197f4b14d97dfe5c7d4adbcebba0c65eebf5
Tomer <Analytics.972@gmail.com>
2024-07-24 18:47:49 +0300

    added padding to raw data for same length tensors

--- sequence_encoder.py
+++ sequence_encoder.py
@@ -1,13 +1,16 @@
 import torch
-
-dna_encoding = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
-rna_encoding = {'A': 0, 'C': 1, 'G': 2, 'U': 3}
+import torch.nn.functional as F
+dna_encoding = {'A': 0, 'C': 1, 'G': 2, 'T': 3, 'X': 4}
+rna_encoding = {'A': 0, 'C': 1, 'G': 2, 'U': 3, 'X': 4}
 ignore = ['N']
+max_len = 41
 
 
 def encode(sequence: str, mapping: dict) -> torch.Tensor:
 
-    digits = torch.Tensor([mapping[x] for x in sequence if x not in ignore])
+    padded_seq = sequence.ljust(max_len, "X")
+    digits = torch.Tensor([mapping[x] for x in padded_seq if x not in ignore])
+
     one_hot = torch.nn.functional.one_hot(digits.to(torch.int64), num_classes=len(mapping))
 
     return one_hot.t()
